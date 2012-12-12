a4d400b54509b7f64d4daebadfd3e9313c61323c
Nicholas Terry <nick.i.terry@gmail.com>
2012-12-11 22:26:56 -0700

    Completed Add method for Buffer.cs.

--- SockLibNG/Buffer/Buffer.cs
+++ SockLibNG/Buffer/Buffer.cs
@@ -8,13 +8,19 @@ namespace SockLibNG.Buffer
 {
     class Buffer
     {
+        private const int BufferSize = 1024;
         private readonly byte[] bytes;
-        private int position;
+        private int position; 
 
         public Buffer()
         {
-            bytes = new byte[1024];
+            bytes = new byte[BufferSize];
             position = 0;
+        }
+
+        public static Buffer New()
+        {
+            return new Buffer();
         }
 
         public void Add(object primitive)
@@ -28,12 +34,28 @@ namespace SockLibNG.Buffer
             }
         }
 
+        public byte[] Get()
+        {
+            return bytes;
+        }
+
         //NOTE: BitConverter class is .NET ONLY AFAIK. In order to be mono compliant, we need to use DataConvert located at http://www.mono-project.com/Mono_DataConvert
         private byte[] ConvertToByteArray(object primitive)
         {
+            if (primitive is bool) return BitConverter.GetBytes((bool)primitive);
+            if (primitive is byte) return BitConverter.GetBytes((byte)primitive);
+            if (primitive is sbyte) return BitConverter.GetBytes((sbyte)primitive);
+            if (primitive is char) return BitConverter.GetBytes((char)primitive);
+            if (primitive is double) return BitConverter.GetBytes((double)primitive);
+            if (primitive is float) return BitConverter.GetBytes((float)primitive);
             if (primitive is int) return BitConverter.GetBytes((int)primitive);
+            if (primitive is uint) return BitConverter.GetBytes((uint)primitive);
+            if (primitive is long) return BitConverter.GetBytes((long)primitive);
+            if (primitive is ulong) return BitConverter.GetBytes((ulong)primitive);
             if (primitive is short) return BitConverter.GetBytes((short)primitive);
-            //TODO: Finish list according to list at http://msdn.microsoft.com/en-us/library/ya5y69ds.aspx
+            if (primitive is ushort) return BitConverter.GetBytes((ushort)primitive);
+            if (primitive is string) return new ASCIIEncoding().GetBytes((string) primitive);
+            throw new DataException("Provided type cannot be serialized for transmission. You must provide a primitive.");
         }
 
         private bool CheckBufferBoundaries(byte[] bytesToCheck)
