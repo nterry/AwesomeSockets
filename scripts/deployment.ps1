b16dc66286efbf1f3a8991b962ebcacf1a9c2b3f
Nicholas Terry <nick.i.terry@gmail.com>
2013-11-26 21:21:32 -0700

    Changed deployment script

--- scripts/deployment.ps1
+++ scripts/deployment.ps1
@@ -14,6 +14,7 @@ Param (
 $nuspec_template=cat .\scripts\AwesomeSockets.dll.template.nuspec
 $a=$a -replace "<version>1.0.0</version>", "<version>$projectVersion.$projectBuildNumber</version>"
 echo $a > "$buildFolder\AwesomeSockets.dll.nuspec"
-.\nuget\NuGet.exe setapikey $variables.Get_Item("SecureNugetKey")
+cd ..\
+.\.nuget\NuGet.exe setapikey $variables.Get_Item("SecureNugetKey")
 .\.nuget\NuGet.exe pack "$buildFolder\AwesomeSockets.dll.nuspec"
-.\nuget\NuGet.exe push "$buildFolder\AwesomeSockets.dll.nupkg"
+.\.nuget\NuGet.exe push "$buildFolder\AwesomeSockets.dll.nupkg"
