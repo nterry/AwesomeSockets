15f34c36d674b5af69358a0a008ac0772cf38091
Nicholas Terry <nick.i.terry@gmail.com>
2013-12-02 18:25:29 -0700

    Added deployment script for AppVeyor CI.

--- scripts/deployment.ps1
+++ scripts/deployment.ps1
@@ -2,6 +2,7 @@ Param (
     $variables = @{},   
     $artifacts = @{},
     $scriptPath,
+	$scriptFolder,
     $buildFolder,
     $srcFolder,
     $outFolder,
@@ -11,10 +12,33 @@ Param (
     $projectBuildNumber
 )
 
-$nuspec_template=cat .\scripts\AwesomeSockets.dll.template.nuspec
-$a=$a -replace "<version>1.0.0</version>", "<version>$projectVersion.$projectBuildNumber</version>"
-echo $a > "$buildFolder\AwesomeSockets.dll.nuspec"
-cd ..\
-.\.nuget\NuGet.exe setapikey $variables.Get_Item("SecureNugetKey")
-.\.nuget\NuGet.exe pack "$buildFolder\AwesomeSockets.dll.nuspec"
-.\.nuget\NuGet.exe push "$buildFolder\AwesomeSockets.dll.nupkg"
+$artifactToUse=$null
+foreach($artifact in $artifacts.values)
+{
+	If ($artifact.name -notmatch ".*\.Tests$")
+	{
+		$artifactToUse=$artifact.sourcePath
+	}
+}
+if ($artifactToUse -eq $null)
+{
+	exit
+}
+
+echo $artifactToUse
+$nugetFolder=".nuget"
+$nugetStage="$tempFolder\nuget-staging"
+md "$nugetStage\lib\Net20"
+md "$nugetStage\lib\Net40"
+
+$nuspecTemplate=cat "$scriptFolder\AwesomeSockets.dll.template.nuspec"
+$nuspecTemplate=$nuspecTemplate -replace "<version>1.0.0</version>", "<version>$projectVersion</version>"
+echo $nuspecTemplate > "$nugetStage\AwesomeSockets.dll.nuspec"
+
+Copy-Item "$artifactToUse\AwesomeSockets.dll" "$nugetStage\lib\Net20"
+Copy-Item "$artifactToUse\AwesomeSockets.dll" "$nugetStage\lib\Net40"
+
+$apiKey=$variables.Get_Item("SecureNugetKey")
+iex "$nugetFolder\NuGet.exe setapikey $apiKey"
+iex "$nugetFolder\NuGet.exe pack $nugetStage\AwesomeSockets.dll.nuspec"
+iex "$nugetFolder\NuGet.exe push $srcFolder\AwesomeSockets.dll.$projectVersion.nupkg"
