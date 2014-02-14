3c5a179c237f85c6029c45b39e653cd92e5b2ed1
Nicholas Terry <nick.i.terry@gmail.com>
2014-02-13 19:32:20 -0700

    Finished basic Acceptance test for TCP synchronous.

--- AwesomeSockets.Tests/AcceptanceTests/TcpSyncTests.cs
+++ AwesomeSockets.Tests/AcceptanceTests/TcpSyncTests.cs
@@ -4,35 +4,49 @@ using AwesomeSockets.Domain.Sockets;
 using AwesomeSockets.Sockets;
 using NUnit.Framework;
 using Buffer = AwesomeSockets.Buffers.Buffer;
+using System.Threading;
 
 namespace AwesomeSockets.Tests.AcceptanceTests
 {
     [TestFixture]
     class TcpSyncTests
     {
-        bool SERVER_EXIT_FLAG;
+        private bool isServerThreadFinished = false;
+        private bool isClientThreadFinished = false;
 
         [Test]
         public void TcpSynchronousAcceptanceTest()
         {
-            SERVER_EXIT_FLAG = false;
-        }
+            Thread serverThread = new Thread(() => {
+                ServerThread(x => {
+                    ServerThreadCallback(x);
+                });
+            });
 
-        private void ServerThread()
-        {
-            var sendBuffer = Buffer.New();
-            var recvBuffer = Buffer.New();
-            var listenSocket = AweSock.TcpListen(14804);
-            var client = AweSock.TcpAccept(listenSocket);
+            Thread clientThread = new Thread(() => {
+                ClientThread(x => {
+                    ClientThreadCallback(x);
+                });
+            });
 
-            SendServerTestMessage(client, sendBuffer);
-            
+            clientThread.Start();
+            serverThread.Start();
+
             do
             {
-            } while (!SERVER_EXIT_FLAG);
+                Thread.Sleep(1000);
+            } while (!isServerThreadFinished && !isClientThreadFinished);
+
+            //Politely ask both threads to stop before main thread dies and hard-aborts them
+            serverThread.Abort();
+            clientThread.Abort();
+
+            //Sleep to allow the threads a chance to die
+            Thread.Sleep(1000);
         }
 
-        private void SendServerTestMessage(ISocket client, Buffer sendBuffer)
+        #region Shared Methods
+        private void SendTestMessage(ISocket other, Buffer sendBuffer)
         {
             Buffer.ClearBuffer(sendBuffer);
             Buffer.Add(sendBuffer, 10);
@@ -43,39 +57,88 @@ namespace AwesomeSockets.Tests.AcceptanceTests
             Buffer.Add(sendBuffer, (byte)255);
             Buffer.FinalizeBuffer(sendBuffer);
 
-            var bytesSent = AweSock.SendMessage(client, sendBuffer);
+            var bytesSent = AweSock.SendMessage(other, sendBuffer);
             Console.WriteLine("Sent payload. {0} bytes written.", bytesSent);
         }
 
-        private void ReceiveServerTestResponse(ISocket client, Buffer recvBuffer)
+        private void ServerThreadCallback(bool isSuccess)
         {
+            isServerThreadFinished = true;
+        }
+
+        private void ClientThreadCallback(bool isSuccess)
+        {
+            isClientThreadFinished = true;
+        }
+        #endregion
+
+        #region Server Methods
+        private void ServerThread(Action<bool> callback)
+        {
+            var sendBuffer = Buffer.New();
+            var recvBuffer = Buffer.New();
+            var listenSocket = AweSock.TcpListen(14804);
+            var client = AweSock.TcpAccept(listenSocket);
+
+            SendTestMessage(client, sendBuffer);
+            callback(ReceiveResponseFromClient(client, recvBuffer));
+        }       
+
+        private bool ReceiveResponseFromClient(ISocket client, Buffer recvBuffer)
+        {
+            bool SERVER_EXIT_FLAG = false;
             do
             {
                 var bytesReceived = AweSock.ReceiveMessage(client, recvBuffer);
                 if (bytesReceived.Item1 > 0)
-                    SERVER_EXIT_FLAG = ValidateServerTestResponse(recvBuffer);
+                {
+                    if (!ValidateResponse(recvBuffer))
+                        return false;
+                    SERVER_EXIT_FLAG = true;
+                }
                 else if (bytesReceived.Item1 == 0)
-                    throw new ApplicationException("Client thread has died prematurely");
+                    return false;
 
-            } while (!SERVER_EXIT_FLAG);           
+                Thread.Sleep(1000);
+            } while (!SERVER_EXIT_FLAG);
+            return true;
         }
 
-        private bool ValidateServerTestResponse(Buffer receiveBuffer)
+        private bool ValidateResponse(Buffer receiveBuffer)
         {
-            throw new NotImplementedException();
+            return ((Buffer.Get<int>(receiveBuffer) == 10) && (Buffer.Get<float>(receiveBuffer) == 20.0F) && (Buffer.Get<double>(receiveBuffer) == 40.0) && (Buffer.Get<char>(receiveBuffer) == 'A') &&
+                (Buffer.Get<string>(receiveBuffer) == "The quick brown fox jumped over the lazy dog") && (Buffer.Get<byte>(receiveBuffer) == ((byte)255)));
         }
+        #endregion
 
-
-        private void TcpAccepted(ISocket socket)
+        #region Client Methods
+        public void ClientThread(Action<bool> callback)
         {
-            _client = socket;
-            SendTestMessage();
+            Buffer sendBuffer = Buffer.New();
+            Buffer recvBuffer = Buffer.New();
+
+            ISocket server = AweSock.TcpConnect("127.0.0.1", 14804);
+
+            ReceiveMessageFromServer(server, recvBuffer);
+            SendTestMessage(server, sendBuffer);
+            callback(true);
         }
 
-        private void MessageReceived(int bytesReceived)
+        private bool ReceiveMessageFromServer(ISocket server, Buffer recvBuffer)
         {
-            Console.WriteLine(string.Format("Received message from client. Size is {0}. Details are as follows: {1} (int)\n{2} (float)\n{3} (double)\n{4} (char)\n{5} (string)\n{6} (byte)", bytesReceived,                                                                                                                                                                      Buffer.Get<string>(_receiveBuffer),
-                                                                                                                                                                                Buffer.Get<byte>(_receiveBuffer)));
+            bool CLIENT_EXIT_FLAG = false;
+            do
+            {
+                var bytesReceived = AweSock.ReceiveMessage(server, recvBuffer);
+                if (bytesReceived.Item1 > 0)
+                    CLIENT_EXIT_FLAG = true;
+                else if (bytesReceived.Item1 == 0)
+                    return false;
+
+                Thread.Sleep(1000);
+            } while (!CLIENT_EXIT_FLAG);
+            return true;
         }
+        #endregion
     }
 }
--- AwesomeSockets.Tests/Program.cs
+++ AwesomeSockets.Tests/Program.cs
@@ -6,17 +6,6 @@ namespace AwesomeSockets.Tests
     {
         static void Main(string[] args)
         {
-            switch (args[0])
-            {
-                case "server":
-                    //new TestServerTcpAsync();
-                    new TestServerUdp();
-                    break;
-                case "client":
-                    //new TestClientTcpAsync();
-                    new TestClientUdp();
-                    break;
-            }
         }
     }
 }
