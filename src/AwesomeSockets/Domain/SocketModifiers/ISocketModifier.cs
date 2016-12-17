52aae9a01303302a892a5b79eb2b352d944928a9
Nicholas Terry <nick.i.terry@gmail.com>
2016-12-16 23:22:30 -0700

    Added .NET Core 1.0 support
    Also, as there are missing pieces to the Socket API,
    I had to rev the major to 2.0 as this is a breaking
    change.
    
    Also took the time to clean up the API a bit.

---
+++ src/AwesomeSockets/Domain/SocketModifiers/ISocketModifier.cs
@@ -0,0 +1,9 @@
+﻿using AwesomeSockets.Domain.Sockets;
+
+namespace AwesomeSockets.Domain.SocketModifiers
+{
+    public interface ISocketModifier
+    {
+        ISocket Apply(ISocket socket);
+    }
+}
---
+++ src/AwesomeSockets/Domain/SocketModifiers/MulticastSocketModifier.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeSockets.Domain.Sockets;
+using AwesomeSockets.Sockets;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AwesomeSockets.Domain.SocketModifiers
+{
+    public class MulticastSocketModifier : ISocketModifier
+    {
+        //These are RFC included values
+        private readonly IPAddress _multicastIpAddress = IPAddress.Parse("224.168.100.2");
+        private readonly int _multicastPort = 11000;
+        
+        public ISocket Apply(ISocket socket)
+        {
+            //TODO: Need to check if socket is UDP as Multicast only works with UDP...
+            AweSock.SetSockOpt(socket, new Dictionary<SocketOptionName, object>
+                {
+                    { SocketOptionName.AddMembership, new MulticastOption(_multicastIpAddress, IPAddress.Parse("127.0.0.1")) }
+                });
+            return socket;
+        }
+    }
+}
